ed034facccc574e691c4322fa8117a6b2b82ef20
myhelpsites-com <gabriel@myhelpsites.com>
2022-01-08 14:33:19 -0300

    certo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.readFile("gabriel.html", (err, data) => {
+fs.readFile("mysite/gabriel.html", (err, data) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(data);
     return res.end();
