d7d2b61e943e226cad3c8f8329fff03dd4dabc31
myhelpsites-com <gabriel@myhelpsites.com>
2022-01-08 17:51:06 -0300

    teste

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-// //index.js
-// const http = require('http');
-// // servidor node.js
-// const fs = require('fs');
-
-// const hostname = "127.0.0.1";
-// const port = 8000;
-
-// const server = http.createServer((req, res) => {
-
-
-//     (async () => {
-//         const db = require("./db");
-//         console.log('Começou!');
-
-//         console.log('SELECT * FROM CLIENTES');
-//         const clientes = await db.selectCustomers();
-//         console.log(clientes[0].nome);
-//         res.writeHead(200, { 'Content-Type': 'application/json' });
-//         // res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write(JSON.stringify(clientes));
-//         // res.write(clientes[0].nome);
-//         return res.end();
-//     })();
-
-
-
-// })
-
-// server.listen(port, hostname, () => {
-//     console.log("servidor rodando")
-// })
-
 const express = require('express') //importacao do pacote
 const app = express() //instanciando express
 app.get('/', function (req, res) { //endereco da requisicao onde e retornado hello world
