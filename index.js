e4000dc4f533db5df9975463034ff5f643926cbb
myhelpsites-com <gabriel@myhelpsites.com>
2022-01-08 14:31:04 -0300

    teste

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const http = require('http');
-// servidor node.js
 const fs = require('fs');
 
-const hostname = "127.0.0.1";
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    fs.readFile("gabriel.html", (err, data) => {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    });
-})
-
+fs.readFile("gabriel.html", (err, data) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    return res.end();
+});
 
 server.listen(port, hostname, () => {
     console.log("servidor rodando")
