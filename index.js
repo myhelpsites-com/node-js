7ec00aeba6d0595e6972655417d99ee6c9b15664
myhelpsites-com <gabriel@myhelpsites.com>
2022-01-08 14:32:21 -0300

    cetro

--- index.js
+++ index.js
@@ -4,8 +4,4 @@ fs.readFile("gabriel.html", (err, data) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(data);
     return res.end();
-});
-
-server.listen(port, hostname, () => {
-    console.log("servidor rodando")
 })
