903312dd8f451216d4014414d25e02517a9513e9
myhelpsites-com <gabriel@myhelpsites.com>
2022-01-08 17:45:55 -0300

    teste

--- aulas/aula3.js
+++ aulas/aula3.js
@@ -41,10 +41,27 @@ app.get('/', function (req, res) { //endereco da requisicao onde e retornado hel
         console.log('SELECT * FROM CLIENTES');
         const clientes = await db.selectCustomers();
         console.log(clientes[0].nome);
-        // res.writeHead(200);
-        // res.writeHead(200, { 'Content-Type': 'text/plain' });
+        // res.writeHead(200, { 'Content-Type': 'application/json' });
+        // res.writeHead(200, { 'Content-Type': 'text/html' });
         // res.write(JSON.stringify(clientes));
-        
+        const alunos = [
+            {
+              nome: 'Aluno1',
+              idade: 12
+            },
+            {
+              nome: 'Aluno2',
+              idade: 13
+            },
+            {
+              nome: 'Aluno3',
+              idade: 14
+            },
+            {
+              nome: 'Aluno4',
+              idade: 15
+            }
+          ]
         res.send(clientes);
         // res.write(clientes[0].nome);
         return res.end();
